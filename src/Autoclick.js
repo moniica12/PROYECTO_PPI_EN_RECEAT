66da4b7c7595754d63677ac445ecbd45266b4726
moniica12 <66142076+moniica12@users.noreply.github.com>
2020-08-06 20:48:17 -0500

    Initial commit

---
+++ src/Autoclick.js
@@ -0,0 +1,66 @@
+import React from "react";
+import "./styles.css";
+
+export default function Autoclick() {
+  return (
+    <div className="Autoclick">
+      <h1>PROYECTO PPI Autoclick</h1>
+      <h2>DATOS DE LOS INTEGRANTES</h2>
+
+     <table border= "1" className="table" >
+       <tr className="fila1">
+
+         <td className="columna">
+           NOMBRES
+           </td>
+           <td className="columna">APELLIDOS</td>
+           <td className="columna">
+             CORREO ELECTRONICO</td>
+             <td className="columna">N° DE CELULAR</td>
+
+             </tr>
+             <tr>
+               <td className="columna1">Monica</td>
+               <td className="columna1">
+                 Hernandez Tabares
+                 </td>
+                 <td className="columna1">
+                   moniiiitabares12@gmail.com 
+                   </td>
+                   <td className="columna1"> 3216434777</td>
+
+                   </tr>
+                   <tr>
+
+                     <td className="columna1"> Sara Yulieth</td>
+                     <td className="columna1">
+                       Osorio Hincapie
+                       </td>
+                       <td className="columna1">
+                         osoriosara0644@gmail.com 
+                         </td>
+                         <td className="columna1"> 3195176502</td>
+                         
+                         </tr>
+                         <tr>
+
+                         <td className="columna1"> Yurani</td>
+                     <td className="columna1">
+                       Grajales Soto
+                       </td>
+                       <td className="columna1">
+                         yuranigra@gmail.com  
+                         </td>
+                         <td className="columna1"> 3218128730</td>
+                         </tr>
+                         </table>
+                         </div>
+  );
+  
+  }
+  
+  
+                         
+
+
+  
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Autoclick from "../src/Autoclick";
+import "./styles.css";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  
+    <Autoclick/>,
+ rootElement
+);
